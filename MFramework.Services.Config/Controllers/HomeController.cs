c6d983c6fc95e3dd001463ba2145e217d612e611
Murat Başeren <muratbaseren@hotmail.com>
2019-01-07 18:07:12 +0300

    Add project files.

---
+++ MFramework.Services.Config/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace MFramework.Services.Config.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.AppSettingsEmailHostValue = ConfigServiceWrapper.ConfigService.GetAppSettingsValue<string>(ConfigKeys.AppSetting_EMailHost);
+            ViewBag.ConnStrDefaultConnStrValue = ConfigServiceWrapper.ConfigService.GetConnectionString(ConfigKeys.ConnStr_DefaultConnStrName);
+            return View();
+        }
+    }
+}
---
+++ MFramework.Services.Config/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MFramework.Services.Config.MvcApplication" Language="C#" %>
